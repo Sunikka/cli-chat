e4b9c8d8b2aea73c82b0825762d9d3d87b6ead12
Sunikka <devsun45@gmail.com>
2024-08-17 15:08:38 +0300

    refactoring + added a login screen UI(only the UI, login not implemented yet)

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"fmt"
+	mainUI "github.com/Sunikka/termitalk/internal/views/main"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/joho/godotenv"
+	"golang.org/x/net/websocket"
 	"log"
 	"math/rand/v2"
 	"os"
-	"strings"
-
-	"github.com/charmbracelet/bubbles/textarea"
-	"github.com/charmbracelet/bubbles/viewport"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-	"github.com/joho/godotenv"
-	"golang.org/x/net/websocket"
 )
 
 var testNames = []string{"Paavo", "Gigachad", "Harold", "Taavi", "MogBot"}
@@ -23,111 +18,10 @@ type wsMsg struct {
 	message string
 }
 
-type model struct {
-	viewport       viewport.Model
-	messages       []string
-	textarea       textarea.Model
-	senderStyle    lipgloss.Style
-	recipientStyle lipgloss.Style
-	ws             *websocket.Conn
-	err            error
-}
-
-type (
-	errMsg error
-)
-
-func InitialModel(ws *websocket.Conn) model {
-	ta := textarea.New()
-	ta.Placeholder = "Send a message..."
-	ta.Focus()
-
-	ta.Prompt = "| "
-	ta.CharLimit = 280
-
-	ta.SetWidth(30)
-	ta.SetHeight(3)
-
-	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
-
-	ta.ShowLineNumbers = false
-
-	vp := viewport.New(30, 5)
-	vp.SetContent(`Welcome to the chat room! Type a message and press enter to send.`)
-
-	ta.KeyMap.InsertNewline.SetEnabled(false)
-
-	return model{
-		textarea:       ta,
-		messages:       []string{},
-		viewport:       vp,
-		senderStyle:    lipgloss.NewStyle().Foreground(lipgloss.Color("36")),
-		recipientStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
-		ws:             ws,
-		err:            nil,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return textarea.Blink
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		tiCmd tea.Cmd
-		vpCmd tea.Cmd
-	)
-
-	m.textarea, tiCmd = m.textarea.Update(msg)
-	m.viewport, vpCmd = m.viewport.Update(msg)
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC, tea.KeyEsc:
-			fmt.Println(m.textarea.Value())
-			return m, tea.Quit
-		case tea.KeyEnter:
-			_, err := m.ws.Write([]byte(m.textarea.Value()))
-			if err != nil {
-				log.Println("Error sending message: ", err)
-			}
-			// 	m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value())
-
-			// For testing
-			// m.messages = append(m.messages, m.recipientStyle.Render("MogBot: ")+"Based")
-
-			// m.viewport.SetContent(strings.Join(m.messages, "\n"))
-			m.textarea.Reset()
-			m.viewport.GotoBottom()
-		}
-	case wsMsg:
-		m.messages = append(m.messages, m.senderStyle.Render(msg.name+": ")+msg.message)
-		m.viewport.SetContent(strings.Join(m.messages, "\n"))
-
-		m.viewport.GotoBottom()
-
-	case errMsg:
-		m.err = msg
-		return m, nil
-	}
-
-	return m, tea.Batch(tiCmd, vpCmd)
-
-}
-
-func (m model) View() string {
-	return fmt.Sprintf(
-		"%s\n\n%s",
-		m.viewport.View(),
-		m.textarea.View(),
-	) + "\n\n"
-}
-
 func main() {
-	err := godotenv.Load()
+	err := godotenv.Load("../../.env")
 	if err != nil {
-		log.Fatal("Error loading environment variables")
+		log.Fatal(err)
 	}
 	// Websocket connection
 	url := os.Getenv("SERVERURL")
@@ -139,15 +33,7 @@ func main() {
 	}
 	defer ws.Close()
 
-	// Print the logo
-	asciiArt, err := os.ReadFile("./assets/ascii_art.txt")
-	if err != nil {
-		log.Println("Error loading the ascii art: ", err)
-	}
-
-	fmt.Println(string(asciiArt))
-
-	p := tea.NewProgram(InitialModel(ws))
+	app := tea.NewProgram(mainUI.NewMainModel(ws), tea.WithAltScreen())
 
 	// Message handler
 	go func() {
@@ -158,13 +44,13 @@ func main() {
 				log.Println("Error reading the message: ", err)
 				return
 			}
-			p.Send(wsMsg{
+			app.Send(wsMsg{
 				name:    testNames[rand.IntN(4)],
 				message: msg})
 		}
 	}()
 
-	_, err = p.Run()
+	_, err = app.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
